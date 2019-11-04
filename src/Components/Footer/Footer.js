c10972adb9cf7d2c319843263c714fe48a4899ae
Ren Lucas <llucas314@gmail.com>
2019-11-04 16:05:37 -0500

    styled nav and footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "./logo.png";
+import "./Footer.css";
 const Footer = () => {
   return (
     <footer>
-      <Link to="/">
-        <h5>National Park Service</h5>
-        <h3>U.S. Department of the Interior</h3>
+      <Link to="/" className="footer-container">
+        <div className="text-container">
+          <h5>National Park Service</h5>
+          <h3>U.S. Department of the Interior</h3>
+        </div>
         <img src={logo} alt="logo" />
       </Link>
     </footer>
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Nav.css";
 const Nav = () => {
   return (
-    <nav>
-      <Link to="/">
+    <nav className="nav">
+      <Link to="/" className="title">
         <h3>National Parks List</h3>
       </Link>
     </nav>
