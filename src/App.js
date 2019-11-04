7cca6943429c476717e696e43d33e4b390d847b5
Ren Lucas <llucas314@gmail.com>
2019-11-04 12:47:52 -0500

    list works

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React, { Component } from "react";
 import "./App.css";
 import Nav from "./Components/Nav/Nav";
 import Footer from "./Components/Footer/Footer";
+import Home from "./Components/Home/Home";
+import parksData from "./data/parks.json";
+import { Route } from "react-router-dom";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      parks: parksData
+    };
+  }
+
   render() {
     return (
       <div>
         <Nav />
-        <main></main>
+        <main>
+          <Route path="/" exact render={() => <Home {...this.state} />} />
+        </main>
         <Footer />
       </div>
     );
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-const Home = () => {
-  return <div></div>;
+const Home = props => {
+  console.log(props.parks);
+  return (
+    <div>
+      {props.parks.map(park => (
+        <div key={park.id}>
+          <img src={park.images[0].url} alt={park.images[0].altText} />
+          <h2>{park.fullName}</h2>
+        </div>
+      ))}{" "}
+    </div>
+  );
 };
 
 export default Home;
