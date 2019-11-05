2b2c3ee7f12e0ecdf2e52ea60a17f8ba76302490
Ren Lucas <llucas314@gmail.com>
2019-11-04 23:31:43 -0500

    styled detaiil component

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -1,20 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Detail.css";
 
 const Details = props => {
   let park = props.parks.filter(park => park.id === props.match.params.id);
   console.log(park);
+
   const details = { ...park[0] };
+
   const address = { ...details.addresses[0] };
 
   return (
-    <div>
-      <img
-        src={details.images[details.images.length - 1].url}
-        alt={details.images[details.images.length - 1].altText}
-      />
-      <h1>{details.name}</h1>
-      <p>{details.description}</p>
+    <div className="details-container">
+      <div
+        className="details-background"
+        style={{
+          backgroundImage: `url(${details.images[details.images.length - 1].url})`
+        }}
+      >
+        <h1>{details.name}</h1>
+      </div>
+
+      <div className="description">
+        <p>{details.description}</p>
+      </div>
       <h2>Address</h2>
       <p>
         {address.line1}
@@ -24,14 +33,19 @@ const Details = props => {
       <h2>Directions</h2>
       <p>{details.directionsInfo}</p>
       <a href={details.url} target="_blank" rel="noopener noreferrer">
-        Read More
+        <div className="button">Read More</div>
       </a>
+      <h2>Images</h2>
       <div className="img-container">
-        {details.images.map((image, i) => (
-          <img key={i} src={image.url} alt={image.altText} />
-        ))}
+        {details.images.map((image, i) =>
+          i < 5 ? <img key={i} src={image.url} alt={image.altText} /> : ""
+        )}
       </div>
-      <Link to="/">Back to Park List</Link>
+      <div className="back">
+        <Link to="/" className="back">
+          Back to Park List
+        </Link>
+      </div>
     </div>
   );
 };
